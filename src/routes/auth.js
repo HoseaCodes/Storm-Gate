d2925add0700b40002a4f812bb1f7e117512356d
Dominique Hosea <66652422+HoseaCodes@users.noreply.github.com>
2024-05-11 08:15:35 -0500

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,1 +1,42 @@
 
+import express from 'express';
+import auth from '../utils/auth.js';
+import isAdmin from '../utils/authAdmin.js';
+import loginRequired from '../utils/loginRequired.js';
+import {
+  register,
+  refreshToken,
+  login,
+  logout,
+  getUser,
+  updateProfile,
+  deleteProfile,
+  getAllUsers,
+  addCart,
+  history
+} from '../controllers/user.js';
+const router = express.Router();
+import {nodecache} from '../utils/cache.js';
+
+
+router.post('/register', register);
+
+router.post('/login', login);
+
+router.post('/logout', logout);
+
+router.get('/refresh_token', refreshToken);
+
+router.get('/info', auth, nodecache, getUser);
+
+router.patch('/addcart', auth, addCart);
+
+router.get('/history', auth, nodecache, history);
+
+router.route('/:id')
+    .get(isAdmin, nodecache, getAllUsers)
+    .put(loginRequired, updateProfile)
+    .delete(loginRequired, deleteProfile)
+
+
+export default router;
