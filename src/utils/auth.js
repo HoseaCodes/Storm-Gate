4965dd1595287a471c8ec1e20434b4fb34503c9e
Dominique Hosea <66652422+HoseaCodes@users.noreply.github.com>
2024-05-11 08:07:51 -0500

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,1 +1,29 @@
 
+import jwt from "jsonwebtoken";
+
+const auth = (req, res, next) => {
+	try {
+		const token = req.header("Authorization");
+		if (!token)
+			return res.status(400).json({ msg: "Invalid Authentication - no token" });
+
+		jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err instanceof jwt.TokenExpiredError) {
+        return res
+        .status(400)
+        .json({ msg: "Token Expired Error", err});
+      }
+      if (err)
+				return res
+					.status(400)
+					.json({ msg: "Invalid Authentication - invalid token"});
+
+			req.user = user;
+			next();
+		});
+	} catch (err) {
+		return res.status(500).json({ msg: err.message });
+	}
+};
+
+export default auth;
