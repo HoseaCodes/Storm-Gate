689daa7d95e808784e5aeec47ac65d859eb9df07
Dominique Hosea <66652422+HoseaCodes@users.noreply.github.com>
2024-05-11 08:14:22 -0500

    Create loginRequired.js

---
+++ src/utils/loginRequired.js
@@ -0,0 +1,5 @@
+const loginRequired = (req, res, next) => {
+  return (req.user || req.params.id ? next() : res.status(401).json({message: 'Unauthorized user'}));
+}
+
+export default loginRequired;
