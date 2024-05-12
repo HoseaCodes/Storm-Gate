42a481922c9f24f6a1a19023b187fecac893dde6
Dominique Hosea <dominique11h@yahoo.com>
2024-05-12 00:44:45 -0500

    fix(model): update headers

--- src/server.js
+++ src/server.js
@@ -21,6 +21,9 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+}); 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({
     extended: true
