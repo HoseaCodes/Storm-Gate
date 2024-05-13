302aad3db2a94fe28e0187cac8d0bacb7d9ff1df
Dominique Hosea <dominique11h@yahoo.com>
2024-05-13 07:12:18 -0500

    fix(model): update db to async

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const URI = process.env.MONGODB_URL || "mongodb://localhost:27017/stormGate";
-const connectDB = () => mongoose
+const connectDB = async () => await mongoose
   .connect(URI)
   .then(() => console.log("Connected to MongoDB"));
 
